01247a8f2621c5aa83537d1c8ccba1e7bbc51b1c
Kossiks <117629075+Kossiks@users.noreply.github.com>
2022-12-23 08:23:11 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,15 @@
+﻿begin
+  var s := readstring('s: ');
+  Assign(input, 'C:\OAIP\13\input.txt');
+  Assign(output, 'C:\OAIP\13\output.txt');
+  while not eof(input) do
+  begin
+    var s1 := ReadString;
+    println(s1);
+  end;
+  println(S);
+  close(input); 
+  close(output); 
+  Erase(input);
+  Rename(output, 'C:\OAIP\13\input.txt');
+end.
---
+++ 4.pas
@@ -0,0 +1,18 @@
+﻿begin
+  var k := readinteger('k: ');
+  Assign(input, 'inputk.txt');
+  Assign(output, 'outputk.txt');
+  var i:=0;
+  while not eof(input) do
+  begin
+    var s := ReadString;
+    inc(i);
+    if i<>(k-1) then 
+      println(s)
+    else
+      println('');
+  end;
+  close(input); 
+  close(output);
+  Rename(output, 'inputCK.txt');
+end.
